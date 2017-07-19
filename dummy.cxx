a0f96b95900108705261881b164d779e2db5b824
Boris Kolpackov <boris@codesynthesis.com>
2017-07-19 17:50:36 +0200

    Initial implementation

---
+++ dummy.cxx
@@ -0,0 +1,5 @@
+// file      : dummy.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2017 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+// Dummy translation unit used to create dummy std.lib for VC.
---
+++ tests/driver.cxx
@@ -0,0 +1,14 @@
+// file      : tests/driver.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2017 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+import std.core;
+import std.io;
+
+using namespace std;
+
+int
+main ()
+{
+  cout << string ("Hello, World!") << endl;
+}
